39cef8ddc0393251d120e3273728969f9e6a5fb8
Daniel A. Wagenaar <daw@caltech.edu>
2025-06-26 09:49:37 -0700

    Added option to "fix" one of the tiles in Placement
    This makes Placement.affine(fix=1) be a close equivalent
    of the removed Optimize class.

--- src/spectralign/placement.py
+++ src/spectralign/placement.py
@@ -75,7 +75,7 @@ class Placement:
         return tilemap
 
     
-    def rigid(self) -> Dict[Tuple, Tuple]:
+    def rigid(self, fix=None) -> Dict[Tuple, Tuple]:
         """Rigid solution with only translation
 
         This places all tiles in model space and returns a dictionary
@@ -83,10 +83,13 @@ class Placement:
 
         The inherent ambiguity in overall position is resolved by
         enforcing the average of all tile positions to lie at (0, 0).
+
+        As an alternative, the `fix` parameter may be used to
+        specify that one particular tile lies at (0, 0).
         """
         tilemap = self._tilemap()
         N = len(tilemap)
-        txy = np.zeros((2, N))
+        txy = []
         for dim in range(2):
             A = np.zeros((N+1,N+1))
             b = np.zeros(N+1)
@@ -111,15 +114,30 @@ class Placement:
                 b[t2] -= w * (x2 - x1)
             A[:-1,-1] = 1
             A[-1,:-1] = 1
-            txy[dim] = np.linalg.solve(A, b)[:-1]
 
+            if fix is not None:
+                tfix = tilemap[fix]
+                i0 = tfix
+                i1 = i0 + 1
+                A = np.delete(A, range(i0, i1), 0)
+                A = np.delete(A, range(i0, i1), 1)
+                b = np.delete(b, range(i0, i1))
+                A = A[:-1,:-1]
+                b = b[:-1]
+            
+            X = np.linalg.solve(A, b)[:-1]
+
+            if fix is not None:
+                X = np.concatenate((X[:i0], [0], X[i1-1:]))
+            txy.append(X)
+            
         rigidpos = {}
         for tid, t in tilemap.items():
-            rigidpos[tid] = (txy[0, t], txy[1, t])
+            rigidpos[tid] = (txy[0][t], txy[1][t])
         return rigidpos
     
     
-    def affine(self) -> Dict[Tuple, Affine]:
+    def affine(self, fix=None) -> Dict[Tuple, Affine]:
         """Rigid solution with affine transformations
 
         This places all tiles in model space and returns a dictionary
@@ -128,14 +146,26 @@ class Placement:
         The inherent ambiguities in scale and overall position are
         resolved by enforcing the average of all tile positions to lie
         at (0, 0) and that the average transformation matrix is
-        unity. This works best if the transformations are relatively
-        close to unity to begin with: Rotations greater than 90°
-        generally do not lead to good results.
+        unity. This works best if the expected transformations are
+        distributed near unity to begin with. For instance, rotations
+        greater than 90° generally do not lead to good results,
+        because averaging matrices is not reasonable in that case. (A
+        theoretically better constraint might be to demand that the
+        determinants of the transforms average to one, but that does
+        not lead to a linear Lagrange multiplier.)
+
+        As an alternative, you can fix one of the transforms to unity
+        using the `fix` parameter. This is only recommended if every
+        tile is strongly connected to the fixed tile with matching
+        points. Otherwise, the solution will bias toward reduced scale
+        for tiles farther from the fixed one.
 
         """
 
         tilemap = self._tilemap()
         N = len(tilemap)
+        M = np.zeros((6*N + 6, 6*N + 6))
+        b = np.zeros((6*N + 6))
 
         def Aindex(t, i, j):
             return t*6 + 2*i + j
@@ -146,61 +176,76 @@ class Placement:
         def Lindex(i):
             return N*6 + 4 + i
 
-        M = np.zeros((6*N + 6, 6*N + 6))
-        b = np.zeros((6*N + 6))
-        for mtch in self.matches:
-            m = mtch.tile1
-            m_ = mtch.tile2
-            p = mtch.p1
-            p_ = mtch.p2
-
+        def addmatch(m, m_, p, p_, w=1):
             # These are the dE/dA terms in A and X
             for n in range(2):
                 for i in range(2):
                     for j in range(2):
-                        M[Aindex(m,n,i), Aindex(m,n,j)] += 2*p[i] * p[j]
-                        M[Aindex(m,n,i), Aindex(m_,n,j)] -= 2*p[i] * p_[j]
-                    M[Aindex(m,n,i), Xindex(m, n)] += 2 * p[i]
-                    M[Aindex(m,n,i), Xindex(m_, n)] -= 2 * p[i]
+                        M[Aindex(m,n,i), Aindex(m,n,j)] += w*p[i] * p[j]
+                        M[Aindex(m,n,i), Aindex(m_,n,j)] -= w*p[i] * p_[j]
+                    M[Aindex(m,n,i), Xindex(m, n)] += w * p[i]
+                    M[Aindex(m,n,i), Xindex(m_, n)] -= w * p[i]
 
             # These are the dE/dX terms in A and X
             for n in range(2):
                 for j in range(2):
-                    M[Xindex(m,n), Aindex(m,n,j)] += 2 * p[j]
-                    M[Xindex(m,n), Aindex(m_,n,j)] -= 2 * p_[j]
-                M[Xindex(m,n), Xindex(m,n)] += 2
-                M[Xindex(m,n), Xindex(m_,n)] -= 2
+                    M[Xindex(m,n), Aindex(m,n,j)] += w * p[j]
+                    M[Xindex(m,n), Aindex(m_,n,j)] -= w * p_[j]
+                M[Xindex(m,n), Xindex(m,n)] += w
+                M[Xindex(m,n), Xindex(m_,n)] -= w
 
-        # These are the dE/dA terms in LL
-        for m in range(N):
+        def addLterms():
+            # These are the dE/dA terms in LL
+            for m in range(N):
+                for i in range(2):
+                    for j in range(2):
+                        M[Aindex(m,i,j), LLindex(i,j)] += 1
+
+            # These are the dE/dX terms in L
+            for m in range(N):
+                for i in range(2):
+                    M[Xindex(m,i), Lindex(i)] += 1
+
+
+            # These are the dE/dLL terms in A
+            for m in range(N):
+                for i in range(2):
+                    for j in range(2):
+                        M[LLindex(i,j), Aindex(m,i,j)] += 1
+
+            # These are the constant dE/dLL terms
             for i in range(2):
-                for j in range(2):
-                    M[Aindex(m,i,j), LLindex(i,j)] += 1
+                b[LLindex(i,i)] += N
 
-        # These are the dE/dX terms in L
-        for m in range(N):
-            for i in range(2):
-                M[Xindex(m,i), Lindex(i)] += 1
+            # These are the dE/dL terms in X
+            for m in range(N):
+                for i in range(2):
+                    M[Lindex(i), Xindex(m,i)] += 1
 
-
-        # These are the dE/dLL terms in A
-        for m in range(N):
-            for i in range(2):
-                for j in range(2):
-                    M[LLindex(i,j), Aindex(m,i,j)] += 1
-
-        # These are the constant dE/dLL terms
-        for i in range(2):
-            b[LLindex(i,i)] += N
-
-        # These are the dE/dL terms in X
-        for m in range(N):
-            for i in range(2):
-                M[Lindex(i), Xindex(m,i)] += 1
-
-        # There are no constant dE/dL terms
-
+            # There are no constant dE/dL terms
+            
+        for mtch in self.matches:
+            addmatch(mtch.tile1, mtch.tile2, mtch.p1, mtch.p2, mtch.weight)
+            addmatch(mtch.tile2, mtch.tile1, mtch.p2, mtch.p1, mtch.weight)
+        addLterms()
+        
+        if fix is not None:
+            tfix = tilemap[fix]
+            i0 = 6*tfix
+            i1 = i0 + 6
+            coefs = M[:, i0:i1]
+            consts = np.sum(coefs * [1,0,0,1,0,0], 1)
+            b -= consts
+            M = np.delete(M, range(i0, i1), 0)
+            M = np.delete(M, range(i0, i1), 1)
+            b = np.delete(b, range(i0, i1))
+            M = M[:-6,:-6]
+            b = b[:-6]
+        
         AX = np.linalg.solve(M, b)
+
+        if fix is not None:
+            AX = np.concatenate((AX[:i0], [1,0,0,1,0,0], AX[i1-6:]))
 
         AA = np.zeros((N,2,2))
         for m in range(N):
